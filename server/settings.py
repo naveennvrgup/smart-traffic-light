2d7cf008c71686a400b5fb35673b63bf21ffad10
Naveen Sundar <naveennvrgup@gmail.com>
2020-09-21 12:08:58 +0530

    feat: collectstatic

--- server/settings.py
+++ server/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT='./static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
