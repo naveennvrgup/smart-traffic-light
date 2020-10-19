2f7a395d488183d322f67ea721669888f8c2fa4a
Naveen Sundar <naveennvrgup@gmail.com>
2020-10-20 00:29:45 +0530

    feat: timer API working flawless 2

--- server/settings.py
+++ server/settings.py
@@ -116,8 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = False
-# TIME_ZONE = 'UTC'
+USE_TZ = True
+TIME_ZONE = 'Asia/Calcutta'
 
 STATIC_URL = '/static/'
 STATIC_ROOT='./static/'
